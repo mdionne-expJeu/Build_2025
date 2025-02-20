77a35e4b5988ffe764df10b220c98efc2068dc55
Mathieu Dionne <mdionne@cmaisonneuve.qc.ca>
2025-02-20 15:04:56 -0500

    test nouvelle version de Unity

--- MushyBounce/Build/MushyBounce.loader.js
+++ MushyBounce/Build/MushyBounce.loader.js
@@ -54,7 +54,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 122731146,
+    wasmFileSize: 123573183,
     cacheControl: function (url) {
       return (url == Module.dataUrl || url.match(/\.bundle/)) ? "must-revalidate" : "no-store";
     },
@@ -1280,6 +1280,7 @@ Module.UnityCache = function () {
       });
   }
 
+
   function downloadFramework() {
       return new Promise(function (resolve, reject) {
         var script = document.createElement("script");
@@ -1336,20 +1337,15 @@ Module.UnityCache = function () {
   // WebGPU is only available if both navigator.gpu exists,
   // and if requestAdapter returns a non-null adapter.
   function checkForWebGPU() {
-    return new Promise(function (resolve, reject) {
-      if (!navigator.gpu) {
-        resolve(false);
-        return;
-      }
-      navigator.gpu.requestAdapter().then(function (adapter) {
-        Module.SystemInfo.hasWebGPU = !!adapter;
-        resolve(Module.SystemInfo.hasWebGPU);
-      });
-    });
+    // WebGPU support was disabled in the build settings.
+    // Skip initialization of WebGPU context.
+    Module.SystemInfo.hasWebGPU = false;
+    return Promise.resolve(false);
   }
 
   function loadBuild() {
     var codeDownloadTimeStartup = performance.now();
+
     downloadFramework().then(function (unityFramework) {
       Module.webAssemblyTimeStart = performance.now();
       unityFramework(Module);
